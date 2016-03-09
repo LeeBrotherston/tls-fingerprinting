e9a9e4569de2588df0e81efde54e8314d68a4c60
leebrotherston <lee@squarelemon.com>
2016-03-08 22:01:45 -0500

    Added GPL licence header for consistency

--- fingerprintls/signal.c
+++ fingerprintls/signal.c
@@ -1,3 +1,29 @@
+/*
+Exciting Licence Info.....
+
+This file is part of FingerprinTLS.
+
+FingerprinTLS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FingerprinTLS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+Exciting Licence Info Addendum.....
+
+FingerprinTLS is additionally released under the "don't judge me" program
+whereby it is forbidden to rip into me too harshly for programming
+mistakes, kthnxbai.
+
+*/
+
 /* This File Is To Cope With Signal Handling Routines */
 
 void sig_handler (int signo);
